85b72d5c9527651eca850733406471bec6a7fbc6
W. Russ Andrews <wrussandrews@protonmail.com>
2024-08-15 19:09:04 -0500

    Updates

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DonutListComponent } from './containers/donut-list/donut-list.component';
+import { DonutCardComponent } from './components/donut-card/donut-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    DonutListComponent,
+    DonutCardComponent
+  ],
+  exports: [
+    DonutListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
