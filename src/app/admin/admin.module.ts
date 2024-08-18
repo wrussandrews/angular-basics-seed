61fe43180a185afb4e17ad9b0f3fae91087ded16
W. Russ Andrews <wrussandrews@protonmail.com>
2024-08-18 18:20:51 -0500

    Updates

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Containers
+
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './containers/donut-single/donut-single.component';
+
+//Components
+
 import { DonutCardComponent } from './components/donut-card/donut-card.component';
-
-
+import { DonutFormComponent } from './components/donut-form/donut-form.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     DonutListComponent,
-    DonutCardComponent
-  ],
-  exports: [
-    DonutListComponent
+    DonutSingleComponent,
+    DonutCardComponent,
+    DonutFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    DonutListComponent, DonutSingleComponent
   ]
 })
 export class AdminModule { }
